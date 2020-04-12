d06dfe4a10927a6642dd0816164b8101a322b31d
Vadym Psarov <vipsarev@gmail.com>
2020-04-13 00:34:47 +0200

    add component to app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+    </v-app-bar>
+
+    <v-content class="align-center indigo lighten-5">
+      <WeatherWidget :data="data" />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import WeatherWidget from "./components/WeatherWidget";
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld
+    WeatherWidget
+  },
+
+  computed: {
+    data() {
+      return this.$store.state.data;
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
