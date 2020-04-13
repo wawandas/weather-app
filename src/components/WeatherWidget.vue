51989f4b55356b7a516d44ba991ef794c18c65c5
Vadym Psarov <vipsarev@gmail.com>
2020-04-13 16:02:23 +0200

    add alert

--- src/components/WeatherWidget.vue
+++ src/components/WeatherWidget.vue
@@ -1,77 +1,88 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="400"
-    :loading="isLoading"
-    :disabled="isLoading"
-  >
-    <v-form class="pa-4">
-      <v-row>
-        <v-col cols="10">
-          <v-text-field
-            name="city"
-            placeholder="Enter city"
-            solo
-            clearable
-            hide-details
-            v-model="city"
-            @keydown.enter.prevent="getWeatherForCity"
-          >
-          </v-text-field>
-        </v-col>
-        <v-col cols="2" class="align-center">
-          <v-btn icon @click.prevent="getWeatherForCoords">
-            <v-icon>mdi-map-marker</v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
-    <div v-if="data.cod === 200">
-      <v-list-item two-line>
-        <v-list-item-content>
-          <v-list-item-title class="headline" data-label="weather-city-name">{{
-            data.name
-          }}</v-list-item-title>
-          <v-list-item-subtitle>
-            {{ data.dt | formatedDate }}, {{ data.weather[0].main }}
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-card-text>
-        <v-row align="center">
-          <v-col cols="6">
-            <span class="display-3">{{ data.main.temp | celsius }}</span>
+  <div>
+    <v-alert
+      v-if="error !== null"
+      border="left"
+      type="warning"
+      close-text="Close Alert"
+      dismissible
+    >
+      Ooops... some error happened, make sure city name is correct.
+    </v-alert>
+    <v-card
+      class="mx-auto"
+      max-width="400"
+      :loading="isLoading"
+      :disabled="isLoading"
+    >
+      <v-form class="pa-4">
+        <v-row>
+          <v-col cols="10">
+            <v-text-field
+              name="city"
+              placeholder="Enter city"
+              solo
+              clearable
+              hide-details
+              v-model="city"
+              @keydown.enter.prevent="getWeatherForCity"
+            >
+            </v-text-field>
           </v-col>
-          <v-col cols="6" class="justify-center">
-            <img :src="getImage()" />
-            <br />
-            ({{ data.weather[0].description }})
+          <v-col cols="2" class="align-center">
+            <v-btn icon @click.prevent="getWeatherForCoords">
+              <v-icon>mdi-map-marker</v-icon>
+            </v-btn>
           </v-col>
         </v-row>
-      </v-card-text>
+      </v-form>
+      <div v-if="data.cod === 200">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline" data-label="weather-city-name"
+              >{{ data.name }}, {{ data.sys.country }}</v-list-item-title
+            >
+            <v-list-item-subtitle>
+              {{ data.dt | formatedDate }}, {{ data.weather[0].main }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon>mdi-send</v-icon>
-        </v-list-item-icon>
-        <v-list-item-subtitle>{{ data.wind.speed }} m/c</v-list-item-subtitle>
-      </v-list-item>
+        <v-card-text>
+          <v-row align="center">
+            <v-col cols="6">
+              <span class="display-3">{{ data.main.temp | celsius }}</span>
+            </v-col>
+            <v-col cols="6" class="justify-center">
+              <img :src="getImage()" />
+              <br />
+              ({{ data.weather[0].description }})
+            </v-col>
+          </v-row>
+        </v-card-text>
 
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon>mdi-cloud-download</v-icon>
-        </v-list-item-icon>
-        <v-list-item-subtitle>{{ data.main.humidity }}%</v-list-item-subtitle>
-      </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-weather-windy</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle>{{ data.wind.speed }} m/c</v-list-item-subtitle>
+        </v-list-item>
 
-      <v-card-actions>
-        <v-btn icon>
-          <v-icon>mdi-cached</v-icon>
-        </v-btn>
-      </v-card-actions>
-    </div>
-  </v-card>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-water</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle>{{ data.main.humidity }}%</v-list-item-subtitle>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn icon>
+            <v-icon>mdi-cached</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -93,6 +104,10 @@ export default {
 
     data() {
       return this.$store.state.data;
+    },
+
+    error() {
+      return this.$store.state.error;
     }
   },
 
